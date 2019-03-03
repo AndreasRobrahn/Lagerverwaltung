c7400d4ff1380ca56a12acb4ef3d2baf61d15728
AndreasRobrahn <40436788+AndreasRobrahn@users.noreply.github.com>
2019-03-03 16:24:40 +0100

    Delete config.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Create a file config.php and insert your database specifics
-return [
-    "server"=>"RUNTIME-199\SQLEXPRESS",
-    
-    "database" => array("Database"=> "Versatel_Schulung",
-					"ReturnDatesAsStrings"=> true)
-];
-?>
