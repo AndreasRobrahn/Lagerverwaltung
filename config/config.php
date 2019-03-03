80d85bbf1fb6a8317693f797e8303cd7828bd990
AndreasRobrahn <40436788+AndreasRobrahn@users.noreply.github.com>
2019-03-03 16:24:26 +0100

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// Create a file config.php and insert your database specifics
+return [
+    "server"=>"RUNTIME-199\SQLEXPRESS",
+    
+    "database" => array("Database"=> "Versatel_Schulung",
+					"ReturnDatesAsStrings"=> true)
+];
+?>
